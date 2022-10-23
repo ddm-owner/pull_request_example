d0cdc63dbaeba30e358cc5e9f1a04cd4a5bee395
ddm-owner <ddm_demo@mail.ru>
2022-10-23 21:34:23 +0300

    Add test console app

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+int Sum(int a, int b)
+{
+    return a + b;
+}
+
+Console.WriteLine("Hello, World!");
+System.Console.WriteLine(Sum(2, 2));
